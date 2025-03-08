1de60f2f555ad4eb7665d23aed114a49fbf987ad
Shiv <sunarshiv21@gmail.com>
2025-03-08 20:42:21 +0545

    claculator

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        alert('Invalid Expression');
+    }
+}
